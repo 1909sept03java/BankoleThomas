1ecc664dddaef6354ec9a18557f3de0ed028637f
Bankole Thomas <bankolethomas@Bankoles-MacBook-Pro-2.local>
2019-09-09 14:09:55 -0400

    mutationChallenge

---
+++ revature/src/challenge1/Mutations.java
@@ -0,0 +1,32 @@
+package challenge1;
+
+
+
+import java.util.Scanner;
+
+
+public class Mutations {
+
+	public static String Change() {
+		
+		String start="AACCGGTT";
+		String end ="AAACGGTA";
+		
+		String [] bank= {"AACCGGTA","AACCGCTA", "AAACGGTA"};
+		int bank2=bank.length;
+		
+		if(start.length()==8){
+			
+		for(int i=0;i<end.length();i++) {
+			if(start.charAt(i) == bank2.charAt(i)){
+				
+			}}
+		
+		return null;}}
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
